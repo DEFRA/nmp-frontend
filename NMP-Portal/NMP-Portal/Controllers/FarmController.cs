b662b498013cef209f8f7b7ff10e7f80408f1f68
Santosh Verma <santosh.verma@RSK-BSL.COM>
2024-04-02 17:54:03 +0530

    Enter Farm address manually

---
+++ NMP-Portal/NMP-Portal/Controllers/FarmController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NMP.Portal.Models;
+
+namespace NMP.Portal.Controllers
+{
+    public class FarmController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ManualAddress()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ManualAddress(FarmAddress farm)
+        {
+            return View();
+        }
+    }
+}
---
+++ NMP-Portal/NMP-Portal/Models/FarmAddress.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using NMP.Portal.Resources;
+
+namespace NMP.Portal.Models
+{
+    public class FarmAddress
+    {
+        [Display(ResourceType = typeof(Resource), Name = nameof(Resource.lblAddressLine1))]
+        public string Address1 { get; set; } = string.Empty;
+
+        [Display(ResourceType = typeof(Resource), Name = nameof(Resource.lblAddressLine2))]
+        public string? Address2 { get; set; } = string.Empty;
+
+        [Display(ResourceType = typeof(Resource), Name = nameof(Resource.lblTownOrCity))]
+        public string? Address3 { get; set; } = string.Empty;
+
+        [Display(ResourceType = typeof(Resource), Name = nameof(Resource.lblCounty))]
+        public string? Address4 { get; set; } = string.Empty;
+
+        [Display(ResourceType = typeof(Resource), Name = nameof(Resource.lblPostCode))]
+        public string PostCode { get; set; } = string.Empty;
+        
+    }
+}
