f8d826dbe451d074b38c0ffed8a0af0eeec92950
Santosh Verma <santosh.verma@RSK-BSL.COM>
2024-04-19 12:26:53 +0530

    minor change

--- NMP-Portal/NMP-Portal/Controllers/FarmController.cs
+++ NMP-Portal/NMP-Portal/Controllers/FarmController.cs
@@ -94,7 +94,7 @@ namespace NMP.Portal.Controllers
             if (_httpContextAccessor.HttpContext != null && _httpContextAccessor.HttpContext.Session.Keys.Contains<string>("FarmData"))
             {
                 model = _httpContextAccessor.HttpContext?.Session.GetObjectFromJson<FarmViewModel>("FarmData");
-                model.OldPostcode = model.Postcode;
+                //model.OldPostcode = model.Postcode;
             }
             return View(model);
         }
@@ -460,7 +460,7 @@ namespace NMP.Portal.Controllers
             }
 
             model.IsCheckAnswer = true;
-            model.OldPostcode = model.Postcode;
+            //model.OldPostcode = model.Postcode;
             _httpContextAccessor.HttpContext?.Session.SetObjectAsJson("FarmData", model);
             return View(model);
 
