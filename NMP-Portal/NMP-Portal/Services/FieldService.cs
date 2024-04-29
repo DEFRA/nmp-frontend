412164133838a33fd92da534b20a2b69acabfd66
Santosh Verma <santosh.verma@RSK-BSL.COM>
2024-04-29 18:43:41 +0530

    Add Farm fields Summary (success) page

--- NMP-Portal/NMP-Portal/Services/FieldService.cs
+++ NMP-Portal/NMP-Portal/Services/FieldService.cs
@@ -399,5 +399,48 @@ namespace NMP.Portal.Services
 
             return isFieldExist;
         }
+        public async Task<List<Field>> FetchFieldsByFarmId(int farmId)
+        {
+            List<Field> fields = new List<Field>();
+            Error error = new Error();
+            try
+            {
+                Token? token = _httpContextAccessor.HttpContext?.Session.GetObjectFromJson<Token>("token");
+                HttpClient httpClient = this._clientFactory.CreateClient("NMPApi");
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + token?.AccessToken);
+                var response = await httpClient.GetAsync(string.Format(APIURLHelper.FetchFieldsByFarmIdAsyncAPI,farmId));
+                string result = await response.Content.ReadAsStringAsync();
+                ResponseWrapper? responseWrapper = JsonConvert.DeserializeObject<ResponseWrapper>(result);
+                if (response.IsSuccessStatusCode)
+                {
+                    if (responseWrapper != null && responseWrapper.Data != null)
+                    {
+                        var fieldslist = responseWrapper.Data.Fields.ToObject<List<Field>>();
+                        fields.AddRange(fieldslist);
+                    }
+                }
+                else
+                {
+                    if (responseWrapper != null && responseWrapper.Error != null)
+                    {
+                        error = responseWrapper.Error.ToObject<Error>();
+                        _logger.LogError($"{error.Code} : {error.Message} : {error.Stack} : {error.Path}");
+                    }
+                }
+            }
+            catch (HttpRequestException hre)
+            {
+                error.Message = Resource.MsgServiceNotAvailable;
+                _logger.LogError(hre.Message);
+                throw new Exception(error.Message, hre);
+            }
+            catch (Exception ex)
+            {
+                error.Message = ex.Message;
+                _logger.LogError(ex.Message);
+                throw new Exception(error.Message, ex);
+            }
+            return fields;
+        }
     }
 }
