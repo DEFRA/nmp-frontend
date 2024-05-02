a6cd58f0847b909a1a6cd0585573736381fa2c92
PoojaTaneja1999 <pooja.taneja@RSK-BSL.com>
2024-05-02 19:08:33 +0530

    Enter Harvest Year , CropGroup and CropType For Plan

---
+++ NMP-Portal/NMP-Portal/Controllers/CropController.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+using NMP.Portal.Helpers;
+using NMP.Portal.Models;
+using NMP.Portal.Resources;
+using NMP.Portal.ServiceResponses;
+using NMP.Portal.Services;
+using NMP.Portal.ViewModels;
+using Error = NMP.Portal.ServiceResponses.Error;
+
+namespace NMP.Portal.Controllers
+{
+    [Authorize]
+    public class CropController : Controller
+    {
+        private readonly ILogger<CropController> _logger;
+        private readonly IDataProtector _farmDataProtector;
+        private readonly IFarmService _farmService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IFieldService _fieldService;
+
+        public CropController(ILogger<CropController> logger, IDataProtectionProvider dataProtectionProvider,
+             IFarmService farmService, IHttpContextAccessor httpContextAccessor, IFieldService fieldService)
+        {
+            _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
+            _farmDataProtector = dataProtectionProvider.CreateProtector("NMP.Portal.Controllers.FarmController");
+            _farmService = farmService;
+            _fieldService = fieldService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateCropPlanCancel(string q)
+        {
+            _httpContextAccessor.HttpContext?.Session.Remove("CropData");
+            return RedirectToAction("FarmSummary", "Farm", new { Id = q });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> HarvestYearForPlan(string q)
+        {
+            CropViewModel model = new CropViewModel();
+            Error error = null;
+            try
+            {
+                if (_httpContextAccessor.HttpContext != null && _httpContextAccessor.HttpContext.Session.Keys.Contains("CropData"))
+                {
+                    model = _httpContextAccessor.HttpContext?.Session.GetObjectFromJson<CropViewModel>("CropData");
+                }
+                if (!string.IsNullOrEmpty(q))
+                {
+                    int farmID = Convert.ToInt32(_farmDataProtector.Unprotect(q));
+                    model.EncryptedFarmId = q;
+
+                    (Farm farm, error) = await _farmService.FetchFarmByIdAsync(farmID);
+                    model.isEnglishRules = farm.EnglishRules;
+                    _httpContextAccessor.HttpContext?.Session.SetObjectAsJson("CropData", model);
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = string.Concat(error.Message == null ? "" : error.Message, ex.Message);
+                return RedirectToAction("FarmSummary", "Farm", new { id = q });
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult HarvestYearForPlan(CropViewModel model)
+        {
+            if (model.Year == null)
+            {
+                ModelState.AddModelError("Year", string.Format(Resource.MsgSelectANameOfFieldBeforeContinuing, Resource.lblHarvestYear.ToLower()));
+                ModelState.AddModelError("Year", Resource.MsgSelectAnOptionBeforeContinuing);
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            _httpContextAccessor.HttpContext.Session.SetObjectAsJson("CropData", model);
+
+            return RedirectToAction("CropGroups");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CropGroups()
+        {
+            CropViewModel model = new CropViewModel();
+            //List<CropGroupResponse> cropGroups = new List<CropGroupResponse>();
+
+            try
+            {
+                if (_httpContextAccessor.HttpContext != null && _httpContextAccessor.HttpContext.Session.Keys.Contains("CropData"))
+                {
+                    model = _httpContextAccessor.HttpContext?.Session.GetObjectFromJson<CropViewModel>("CropData");
+                }
+                ViewBag.CropGroupList = await _fieldService.FetchCropGroups();
+                //ViewBag.CropGroupList = cropGroups;
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = ex.Message;
+                return RedirectToAction("HarvestYearForPlan");
+            }
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CropGroups(CropViewModel model)
+        {
+            if (model.CropGroupId == null)
+            {
+                ModelState.AddModelError("CropGroupId", string.Format(Resource.MsgSelectANameOfFieldBeforeContinuing, Resource.lblCropGroup.ToLower()));
+            }
+            if (!ModelState.IsValid)
+            {                
+                ViewBag.CropGroupList = await _fieldService.FetchCropGroups();
+                return View(model);
+            }
+
+            if (_httpContextAccessor.HttpContext != null && _httpContextAccessor.HttpContext.Session.Keys.Contains("CropData"))
+            {
+                CropViewModel CropData = _httpContextAccessor.HttpContext?.Session.GetObjectFromJson<CropViewModel>("CropData");
+                if (CropData.CropGroupId != model.CropGroupId)
+                {
+                    model.CropType = string.Empty;
+                    model.CropTypeID = null;
+                }
+            }
+
+            model.CropGroup = await _fieldService.FetchCropGroupById(model.CropGroupId.Value);
+            _httpContextAccessor.HttpContext.Session.SetObjectAsJson("CropData", model);
+
+            return RedirectToAction("CropTypes");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CropTypes()
+        {
+            CropViewModel model = new CropViewModel();
+            List<CropTypeResponse> cropTypes = new List<CropTypeResponse>();
+
+            try
+            {
+                if (_httpContextAccessor.HttpContext != null && _httpContextAccessor.HttpContext.Session.Keys.Contains("CropData"))
+                {
+                    model = _httpContextAccessor.HttpContext?.Session.GetObjectFromJson<CropViewModel>("CropData");
+                }
+
+                cropTypes = await _fieldService.FetchCropTypes(model.CropGroupId ?? 0);
+                var country = model.isEnglishRules ? (int)NMP.Portal.Enums.Country.England : (int)NMP.Portal.Enums.Country.Scotland;
+                var cropTypeList = cropTypes.Where(x => x.CountryId == country || x.CountryId == (int)NMP.Portal.Enums.Country.Both).ToList();
+
+                ViewBag.CropTypeList = cropTypeList;
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = ex.Message;
+                return RedirectToAction("CropGroups");
+            }
+
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CropTypes(CropViewModel model)
+        {
+            if (model.CropTypeID == null)
+            {
+                ModelState.AddModelError("CropTypeID", string.Format(Resource.MsgSelectANameOfFieldBeforeContinuing, Resource.lblCropType.ToLower()));
+            }
+            if (!ModelState.IsValid)
+            {
+                List<CropTypeResponse> cropTypes = new List<CropTypeResponse>();
+                cropTypes = await _fieldService.FetchCropTypes(model.CropGroupId ?? 0);
+                var country = model.isEnglishRules ? (int)NMP.Portal.Enums.Country.England : (int)NMP.Portal.Enums.Country.Scotland;
+                ViewBag.CropTypeList = cropTypes.Where(x => x.CountryId == country || x.CountryId == (int)NMP.Portal.Enums.Country.Both).ToList();
+                return View(model);
+            }
+            model.CropType = await _fieldService.FetchCropTypeById(model.CropTypeID.Value);
+            _httpContextAccessor.HttpContext.Session.SetObjectAsJson("CropData", model);
+
+            return RedirectToAction("CropTypes");
+        }
+    }
+}
