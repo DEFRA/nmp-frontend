a0b8fdd048c9b8e5e316fa247caab0fc1e8e45ea
PoojaTaneja1999 <pooja.taneja@RSK-BSL.com>
2024-08-13 12:33:34 +0530

    LastHarvestYear added.
    NMPT 575 user story done

--- NMP-Portal/NMP-Portal/Controllers/FieldController.cs
+++ NMP-Portal/NMP-Portal/Controllers/FieldController.cs
@@ -108,7 +108,7 @@ namespace NMP.Portal.Controllers
                     (Farm farm, error) = await _farmService.FetchFarmByIdAsync(model.FarmID);
                     model.isEnglishRules = farm.EnglishRules;
                     model.FarmName = farm.Name;
-                    //model.HarvestYear = farm.HarvestYear;
+                    model.LastHarvestYear = farm.LastHarvestYear;
                 }
             }
             catch (Exception ex)
