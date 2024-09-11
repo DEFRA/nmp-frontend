0c505400261546fa6c0eba6bbd966c15e63fe9ad
Santosh Verma <santosh.verma@RSK-BSL.COM>
2024-09-11 14:51:48 +0530

    changes  to check Slurry or poultry manure exist within last 20 days (api modified)

--- NMP-Portal/NMP-Portal/Services/IOrganicManureService.cs
+++ NMP-Portal/NMP-Portal/Services/IOrganicManureService.cs
@@ -39,6 +39,6 @@ namespace NMP.Portal.Services
         Task<(CropTypeLinkingResponse, Error)> FetchCropTypeLinkingByCropTypeId(int cropTypeId);
         Task<(List<int>, Error)> FetchManureTypsIdsByFieldIdYearAndConfirmFromOrgManure(int fieldId, int year,bool confirm);
         Task<(decimal, Error)> FetchTotalNBasedOnManIdFromOrgManureAndFertiliser(int managementId, bool confirm);
-        Task<(bool, Error)> FetchOrganicManureExistanceByDateRange(string dateFrom, string dateTo, int manureTypeId, bool isLiquid);
+        Task<(bool, Error)> FetchOrganicManureExistanceByDateRange(string dateFrom, string dateTo, bool isConfirm);
     }
 }
