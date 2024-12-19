10730a1c729ef40c685666b8e368f0d98337663c
PoojaTaneja1999 <pooja.taneja@RSK-BSL.com>
2024-12-19 19:32:30 +0530

    nmpt-889 user story done

---
+++ NMP-Portal/NMP-Portal/Controllers/ReportController.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NMP.Portal.Helpers;
+using NMP.Portal.Models;
+using NMP.Portal.Resources;
+using NMP.Portal.ServiceResponses;
+using NMP.Portal.Services;
+using NMP.Portal.ViewModels;
+using Error = NMP.Portal.ServiceResponses.Error;
+
+namespace NMP.Portal.Controllers
+{
+    [Authorize]
+    public class ReportController : Controller
+    {
+        private readonly ILogger<ReportController> _logger;
+        private readonly IDataProtector _dataProtector;
+        private readonly IDataProtector _farmDataProtector;
+        private readonly IAddressLookupService _addressLookupService;
+        private readonly IUserFarmService _userFarmService;
+        private readonly IFarmService _farmService;
+        private readonly IFieldService _fieldService;
+        private readonly ICropService _cropService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public ReportController(ILogger<ReportController> logger, IDataProtectionProvider dataProtectionProvider, IHttpContextAccessor httpContextAccessor, IAddressLookupService addressLookupService,
+            IUserFarmService userFarmService, IFarmService farmService,
+            IFieldService fieldService, ICropService cropService)
+        {
+            _logger = logger;
+            _dataProtector = dataProtectionProvider.CreateProtector("NMP.Portal.Controllers.ReportController");
+            _farmDataProtector = dataProtectionProvider.CreateProtector("NMP.Portal.Controllers.FarmController");
+            _addressLookupService = addressLookupService;
+            _userFarmService = userFarmService;
+            _farmService = farmService;
+            _fieldService = fieldService;
+            _cropService = cropService;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public IActionResult Index()
+        {
+            _logger.LogTrace($"Report Controller : Index() action called");
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> ExportFields(string i, string j)
+        {
+            _logger.LogTrace("Crop Controller : ExportFields() action called");
+            ReportViewModel model = null;
+            try
+            {
+                if (_httpContextAccessor.HttpContext != null && _httpContextAccessor.HttpContext.Session.Keys.Contains("ReportData"))
+                {
+                    model = new ReportViewModel();
+                    model = _httpContextAccessor.HttpContext?.Session.GetObjectFromJson<ReportViewModel>("ReportData");
+                }
+                else if (string.IsNullOrWhiteSpace(i) && string.IsNullOrWhiteSpace(j))
+                {
+                    return RedirectToAction("FarmList", "Farm");
+                }
+                if (model == null)
+                {
+                    model = new ReportViewModel();
+                    if (!(string.IsNullOrWhiteSpace(i) && string.IsNullOrWhiteSpace(j)))
+                    {
+
+                        model.EncryptedFarmId = i;
+                        model.EncryptedHarvestYear = j;
+                        model.FarmId = Convert.ToInt32(_farmDataProtector.Unprotect(model.EncryptedFarmId.ToString()));
+                        model.Year = Convert.ToInt32(_farmDataProtector.Unprotect(model.EncryptedHarvestYear.ToString()));
+                        //int farmID = Convert.ToInt32(_farmDataProtector.Unprotect(model.EncryptedFarmId));
+                        (Farm farm, Error error) = await _farmService.FetchFarmByIdAsync(model.FarmId.Value);
+                        if (farm != null)
+                        {
+                            model.FarmName = farm.Name;
+                        }
+                    }
+                }
+                if (model != null && model.FarmId != null)
+                {
+                    (List<HarvestYearPlanResponse> fieldList, Error error) = await _cropService.FetchHarvestYearPlansByFarmId(model.Year.Value, model.FarmId.Value);
+                    if (string.IsNullOrWhiteSpace(error.Message))
+                    {
+                        var SelectListItem = fieldList.Select(f => new SelectListItem
+                        {
+                            Value = f.FieldID.ToString(),
+                            Text = f.FieldName
+                        }).ToList();
+                        ViewBag.fieldList = SelectListItem;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogTrace($"Report Controller : Exception in ExportFields() action : {ex.Message}, {ex.StackTrace}");
+                TempData["ErrorOnHarvestYearOverview"] = ex.Message;
+                return RedirectToAction("HarvestYearOverview", new
+                {
+                    id = model.EncryptedFarmId,
+                    year = model.EncryptedHarvestYear
+                });
+            }
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ExportFields(ReportViewModel model)
+        {
+            _logger.LogTrace("Report Controller : ExportFields() post action called");
+            try
+            {
+                int farmID = Convert.ToInt32(_farmDataProtector.Unprotect(model.EncryptedFarmId));
+                (List<HarvestYearPlanResponse> fieldList, Error error) = await _cropService.FetchHarvestYearPlansByFarmId(model.Year.Value, model.FarmId.Value);
+                if (string.IsNullOrWhiteSpace(error.Message))
+                {
+                    var selectListItem = fieldList.Select(f => new SelectListItem
+                    {
+                        Value = f.FieldID.ToString(),
+                        Text = f.FieldName
+                    }).ToList();
+
+                    if (model.FieldList == null || model.FieldList.Count == 0)
+                    {
+                        ModelState.AddModelError("FieldList", string.Format(Resource.MsgSelectANameOfFieldBeforeContinuing, Resource.lblField.ToLower()));
+                    }
+                    if (!ModelState.IsValid)
+                    {
+                        ViewBag.fieldList = selectListItem;
+                        return View("ExportFields", model);
+                    }
+                    if (model.FieldList.Count == 1 && model.FieldList[0] == Resource.lblSelectAll)
+                    {
+                        model.FieldList = selectListItem.Select(item => item.Value).ToList();
+                    }
+                    _httpContextAccessor.HttpContext.Session.SetObjectAsJson("ReportData", model);
+
+                    return RedirectToAction("CropAndFieldManagement");
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogTrace($"Report Controller : Exception in ExportFields() post action : {ex.Message}, {ex.StackTrace}");
+                TempData["ErrorOnSelectField"] = ex.Message;
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult CropAndFieldManagement()
+        {
+            ReportViewModel model = new ReportViewModel();
+            if (_httpContextAccessor.HttpContext != null && _httpContextAccessor.HttpContext.Session.Keys.Contains("ReportData"))
+            {
+                model = _httpContextAccessor.HttpContext?.Session.GetObjectFromJson<ReportViewModel>("ReportData");
+            }
+            else
+            {
+                return RedirectToAction("FarmList", "Farm");
+            }
+            _logger.LogTrace("Report Controller : CropAndFieldManagement() post action called");
+            return View(model);
+        }
+    }
+}
