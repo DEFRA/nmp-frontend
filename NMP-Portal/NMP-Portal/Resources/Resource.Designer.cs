d558579d63032ddccea0cee2547d2c58c033e142
PoojaTaneja1999 <pooja.taneja@RSK-BSL.com>
2025-07-19 16:14:23 +0530

    remove no use resource

--- NMP-Portal/NMP-Portal/Resources/Resource.Designer.cs
+++ NMP-Portal/NMP-Portal/Resources/Resource.Designer.cs
@@ -6255,15 +6255,6 @@ namespace NMP.Portal.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to m³.
-        /// </summary>
-        public static string lblMeterCubic {
-            get {
-                return ResourceManager.GetString("lblMeterCubic", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Metric units.
         /// </summary>
         public static string lblMetricUnits {
