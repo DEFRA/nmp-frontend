d362b0b5be449c380d2bb5c79f3d5d80276bbfc0
Anil Yadav <Anil.Yadav@RSK-BSL.COM>
2025-11-10 17:25:30 +0530

    Removed unused code

--- NMP-Portal/NMP-Portal/Services/IPKBalanceService.cs
+++ NMP-Portal/NMP-Portal/Services/IPKBalanceService.cs
@@ -6,7 +6,7 @@ namespace NMP.Portal.Services
     public interface IPKBalanceService : IService
     {
         Task<PKBalance> FetchPKBalanceByYearAndFieldId(int year,int fieldId);
-        Task<(PKBalance,Error Error)> AddPKBalance(string pkBalanceData);
-        Task<(PKBalance, Error Error)> UpdatePKBalance(string pkBalanceData);
+        //Task<(PKBalance,Error Error)> AddPKBalance(string pkBalanceData);
+        //Task<(PKBalance, Error Error)> UpdatePKBalance(string pkBalanceData);
     }
 }
