6d99933a2dc7ce7fb59290b4aa8fa407db26aaad
Anil Yadav <Anil.Yadav@RSK-BSL.COM>
2025-12-19 22:02:26 +0530

    in progress

---
+++ NMP-Portal/NMP-Businesses/FarmLogic.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using NMP.Application;
+using NMP.Commons.Models;
+namespace NMP.Businesses;
+
+public class FarmLogic(ILogger<FarmLogic> logger) : IFarmLogic
+{
+    private readonly ILogger<FarmLogic> _logger = logger;
+    public Task<Farm?> FetchFarmByIdAsync(int farmId)
+    {
+        _logger.LogTrace("FarmLogic : FetchFarmByIdAsync() called");
+        throw new NotImplementedException();
+    }
+}
