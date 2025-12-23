1074b61dc8621322c6cc44d2b1bb6a4c3ff2e3d4
Anil Yadav <Anil.Yadav@RSK-BSL.COM>
2025-12-23 15:01:40 +0530

    Error fixed

--- NMP-Portal/NMP-Businesses/AboutServiceLogic.cs
+++ NMP-Portal/NMP-Businesses/AboutServiceLogic.cs
@@ -4,28 +4,26 @@ using NMP.Application;
 using NMP.Commons.Models;
 using NMP.Core.Attributes;
 using NMP.Core.Interfaces;
+namespace NMP.Businesses;
 
-namespace NMP.Businesses
+[Business(ServiceLifetime.Transient)]
+public class AboutServiceLogic(ILogger<AboutServiceLogic> logger, IUserExtensionService userExtensionService) : IAboutServiceLogic
 {
-    [Business(ServiceLifetime.Transient)]
-    public class AboutServiceLogic(ILogger<AboutServiceLogic> logger, IUserExtensionService userExtensionService) : IAboutServiceLogic
+    private readonly ILogger<AboutServiceLogic> _logger = logger;
+    private readonly IUserExtensionService _userExtensionService = userExtensionService;
+
+    public async Task<bool> CheckDoNotShowAboutThisService()
     {
-        private readonly ILogger<AboutServiceLogic> _logger = logger;
-        private readonly IUserExtensionService _userExtensionService = userExtensionService;
+        _logger.LogTrace("AboutServiceLogic : HasDoNotShowAboutThisService() called");
+        UserExtension? userExtension = await _userExtensionService.FetchUserExtensionAsync();
+        return userExtension != null && userExtension.DoNotShowAboutThisService;
+    }
 
-        public async Task<bool> CheckDoNotShowAboutThisService()
-        {
-            _logger.LogTrace("AboutServiceLogic : HasDoNotShowAboutThisService() called");
-            UserExtension? userExtension = await _userExtensionService.FetchUserExtensionAsync();
-            return userExtension != null && userExtension.DoNotShowAboutThisService;
-        }
-
-        public async Task<bool> UpdateShowAboutServiceAsync(bool doNotShowAboutThisService)
-        {
-            _logger.LogTrace("AboutServiceLogic : UpdateShowAboutServiceAsync() called");
-            AboutService aboutService = new() { DoNotShowAboutThisService = doNotShowAboutThisService };
-            UserExtension? userExtension = await _userExtensionService.UpdateShowAboutServiceAsync(aboutService);
-            return userExtension != null && userExtension.DoNotShowAboutThisService;
-        }
+    public async Task<bool> UpdateShowAboutServiceAsync(bool doNotShowAboutThisService)
+    {
+        _logger.LogTrace("AboutServiceLogic : UpdateShowAboutServiceAsync() called");
+        AboutService aboutService = new() { DoNotShowAboutThisService = doNotShowAboutThisService };
+        UserExtension? userExtension = await _userExtensionService.UpdateShowAboutServiceAsync(aboutService);
+        return userExtension != null && userExtension.DoNotShowAboutThisService;
     }
 }
