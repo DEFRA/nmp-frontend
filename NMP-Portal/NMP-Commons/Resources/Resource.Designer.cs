056162309826c9940f945e54422fd5d078e6c9ae
Anil Yadav <Anil.Yadav@RSK-BSL.COM>
2026-03-13 17:56:22 +0530

    feature- 2867

--- NMP-Portal/NMP-Commons/Resources/Resource.Designer.cs
+++ NMP-Portal/NMP-Commons/Resources/Resource.Designer.cs
@@ -16588,7 +16588,7 @@ namespace NMP.Commons.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to What is the estimated angle of the bank slope?.
+        ///   Looks up a localized string similar to What is the bank slope?.
         /// </summary>
         public static string lblWhatIsTheEstimatedAngleOfTheBankSlope {
             get {
