050e0ec6f90693b1c4274967f1ab68e80b025e23
Santosh Verma <santosh.verma@RSK-BSL.COM>
2026-03-18 20:43:57 +0530

    sonar issues

--- NMP-Portal/NMP-Services/FertiliserManureService.cs
+++ NMP-Portal/NMP-Services/FertiliserManureService.cs
@@ -641,7 +641,10 @@ public class FertiliserManureService : Service, IFertiliserManureService
             }
             else
             {
-                _logger.LogError("{Code} : {Message} : {Stack} : {Path}", error.Code, error.Message, error.Stack, error.Path);
+                if(error != null)
+                {
+                    _logger.LogError("{Code} : {Message} : {Stack} : {Path}", error.Code, error.Message, error.Stack, error.Path);
+                }
             }
         }
         catch (HttpRequestException hre)
