61e1ac6c731587511a3f19065b52f34b8ca57d82
Pooja <pooja.taneja@rsk-bsl.com>
2026-07-01 18:26:52 +0530

    minor change

--- NMP-Portal/NMP-Commons/Resources/Resource.Designer.cs
+++ NMP-Portal/NMP-Commons/Resources/Resource.Designer.cs
@@ -19,7 +19,7 @@ namespace NMP.Commons.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resource {
@@ -10326,7 +10326,7 @@ namespace NMP.Commons.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Mineralizated nitrogen (N).
+        ///   Looks up a localized string similar to Mineralisated nitrogen (N).
         /// </summary>
         public static string lblMineralisedNitrogen {
             get {
@@ -10650,7 +10650,7 @@ namespace NMP.Commons.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Nitrogen (N) mineralization and losses.
+        ///   Looks up a localized string similar to Nitrogen (N) mineralisation and losses.
         /// </summary>
         public static string lblNitrogenMineralizationAndLosses {
             get {
