47d470961340dd7f62049dd1a399ab728823e4d5
Santosh Verma <santosh.verma@RSK-BSL.COM>
2026-07-08 21:32:06 +0530

    manner report page

---
+++ NMP-Portal/NMP-Commons/ViewModels/MannerManureAnalysisViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMP.Commons.ViewModels
+{
+    public class MannerManureAnalysisViewModel
+    {
+        public decimal? DryMatterContent { get; set; }
+        public decimal? TotalNitrogen { get; set; }
+        public decimal? AmmoniumNitrogen { get; set; }
+        public decimal? UricAcidNitrogen { get; set; }
+        public decimal? NitrateNitrogen { get; set; }
+        public decimal? TotalPhosphate { get; set; }
+        public decimal? TotalPotash { get; set; }
+        public decimal? TotalSulphur { get; set; }
+        public decimal? TotalMagnesium { get; set; }
+    }
+}
