01d9a16a08b5beeaa4d9a04a9d039a1050e11748
Anil Yadav <Anil.Yadav@RSK-BSL.COM>
2026-07-13 15:51:54 +0530

    fix

--- NMP-Portal/NMP-Commons/Resources/Resource.Designer.cs
+++ NMP-Portal/NMP-Commons/Resources/Resource.Designer.cs
@@ -19,7 +19,7 @@ namespace NMP.Commons.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resource {
@@ -13594,7 +13594,7 @@ namespace NMP.Commons.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Plan organic and inorganic material applications, which include MANNER-NPK calculations.
+        ///   Looks up a localized string similar to Plan organic material and inorganic fertiliser applications.
         /// </summary>
         public static string lblReleaseScheduleContentSix {
             get {
