8876090930e14ca4a75364b594e12d61480a0211
Pooja <pooja.taneja@rsk-bsl.com>
2026-07-16 12:15:10 +0530

    in process

--- NMP-Portal/NMP-Commons/Resources/Resource.Designer.cs
+++ NMP-Portal/NMP-Commons/Resources/Resource.Designer.cs
@@ -19,7 +19,7 @@ namespace NMP.Commons.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resource {
@@ -7792,6 +7792,15 @@ namespace NMP.Commons.Resources {
         public static string lblHowMuchOfEachNutrientDoYouPlanforUpdate {
             get {
                 return ResourceManager.GetString("lblHowMuchOfEachNutrientDoYouPlanforUpdate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to How to enter the application rate.
+        /// </summary>
+        public static string lblHowToEnterTheApplicationRate {
+            get {
+                return ResourceManager.GetString("lblHowToEnterTheApplicationRate", resourceCulture);
             }
         }
         
